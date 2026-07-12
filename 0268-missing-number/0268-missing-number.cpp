b5524fb6a318f6dee79503103205ec5d377126af
atrihazra <atrihazra09@gmail.com>
2026-07-13 01:41:28 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.85%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size(), sum=0;
+
+        int Maxi=n*(n+1)/2;
+
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+
+        return Maxi-sum;
+    
+    }    
+};
