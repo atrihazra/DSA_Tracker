2975513333c0bb68b3feef5eedd3221adbe097bf
atrihazra <atrihazra09@gmail.com>
2026-07-24 05:44:25 +0530

    Time: 4 ms (51.5%), Space: 14.9 MB (20.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+    
+    //USING HASHMAPS
         map <int, int> mpp;
         for(int i=0;i<nums.size();i++){
             int x=nums[i];
@@ -10,5 +12,20 @@ public:
             mpp[x]=i;
         }
         return {-1,-1};
+
+    // //USING 2 POINTER FOR VALUE NOT INDEX
+    // int n=nums.size();
+    // sort(nums.begin(), nums.end());
+    // int left=0,right=n-1;
+
+    // while(left<right){
+    //     if(nums[left]+nums[right] < target)
+    //         left++;
+    //     else if(nums[left]+nums[right] > target)
+    //         right--;
+    //     else if(nums[left]+nums[right]==target)
+    //         return {nums[left],nums[right]};
+    // }
+    // return {-1,-1};
     }
 };
