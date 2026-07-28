56732795d65fbb1f8fb41c9101caee567cd015ef
atrihazra <atrihazra09@gmail.com>
2026-07-29 02:44:56 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,12 +7,14 @@ public:
         while (mid <= high) {
             if (nums[mid] == 0) {
                 swap(nums[mid], nums[low]);
-                 low++, mid++;
-            } else if (nums[mid] == 1)
+                low++, mid++;
+            }
+            else if (nums[mid] == 1)
                 mid++;
+
             else {
                 swap(nums[mid], nums[high]);
-                 high--;
+                high--;
             }
         }
     }
