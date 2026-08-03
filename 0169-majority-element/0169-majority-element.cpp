65f8abfc5ed3a7294530677ef0fd22b4671f7675
atrihazra <atrihazra09@gmail.com>
2026-08-04 01:36:42 +0530

    Time: 0 ms (100%), Space: 28.2 MB (40.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map <int,int> mpp;
-        int n=nums.size();
+        
+        int freq=0;//votes cancel,majority element will never have negative votes
 
-        for(int i=0;i<n;i++)
-            mpp[nums[i]]++;
+        int ans=0;
 
-        for(auto it:mpp){
-            if(it.second > n/2)
-                return it.first;
+        for(int i=0;i<nums.size();i++){
+            if(freq==0)
+                ans=nums[i];
+            if(ans==nums[i])
+                freq++;
+            else freq--;
         }
-        return -1; //using hashing
+        return ans;
+
+
+        
+        //USING HASHING
+        
+        // map <int,int> mpp;
+        // int n=nums.size();
+
+        // for(int i=0;i<n;i++)
+        //     mpp[nums[i]]++;
+        
+        
+
+        // for(auto it:mpp){
+        //     if(it.second > n/2)
+        //         return it.first;
+        // }
+        // return -1; 
     }
 };
